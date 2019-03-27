a0e7b95040f4b8250c7cb8c6b09e9184ad08dfcf
Yuri Gor <yuri.horus@gmail.com>
2019-03-27 21:45:39 +0300

    test: special characters

--- test/default.js
+++ test/default.js
@@ -122,4 +122,15 @@ describe('Default plugin usage', function() {
     );
     doc.should.have.property('uniqueSlug', tellme.getSlug(4));
   });
+
+  it('special characters', async ()=>{
+    let doc = await ShortId.create({
+      title: "I hate this #^(@ special characters. Remove|replace&don't show me"
+    });
+    doc.should.have
+      .property('slug')
+      .and.equal(
+        'i-hate-this-special-characters-remove-or-replace-and-don-t-show-me'
+      );
+  });
 });
---
+++ test/special.js
@@ -0,0 +1,40 @@
+'use strict';
+const mongoose = require('mongoose');
+const { options, slugPaddingSize, nIterations } = require('./options');
+const slugGenerator = require('../.');
+options.symbols=false;
+mongoose.plugin(slugGenerator, options);
+
+const SpecialResourceShortId = new mongoose.Schema({
+  title: { type: String },
+  subtitle: { type: String },
+  slug: { type: String, slug: ['title', 'subtitle'] }
+});
+
+const
+  chai = require('chai'),
+  should = chai.should(),
+  assert = require('assert');
+
+const ShortId = mongoose.model('SpecialResourceShortId', SpecialResourceShortId);
+
+describe('No Special Chars', function() {
+  before(async () => {
+    await ShortId.remove({});
+  });
+
+  after(async () => {
+    await ShortId.remove({});
+  });
+
+  it('special characters', async ()=>{
+    let doc = await ShortId.create({
+      title: "I hate this #^(@ special characters. Remove|replace&don't show me"
+    });
+    doc.should.have
+      .property('slug')
+      .and.equal(
+        'i-hate-this-special-characters-remove-replace-don-t-show-me'
+      );
+  });
+});
