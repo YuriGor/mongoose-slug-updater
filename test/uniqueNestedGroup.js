67ce22514e1b3f6947d62e6a08cd4b04a8f44c89
Myk Willis <myk@mykwillis.com>
2021-01-15 15:22:45 -0800

    Fix for issue #25: handling permanent, document-scoped slugs in subdocument array.

--- test/uniqueNestedGroup.js
+++ test/uniqueNestedGroup.js
@@ -21,6 +21,22 @@ const childSchema = mongoose.Schema({
   },
 });
 
+const childPermenantSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  slug: {
+    type: String,
+    slug: 'title',
+    index: true,
+    permanent: true,
+    slugPaddingSize: 4,
+    uniqueGroupSlug: '/_id',
+  },
+});
+
 const scratchSchema = mongoose.Schema({
   title: {
     type: String,
@@ -52,7 +68,17 @@ const scratchSchema = mongoose.Schema({
   // ],
 });
 
+const scratchPermanentSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  children: [childPermenantSchema],
+});
+
 const Scratch = mongoose.model('scratchSchema', scratchSchema);
+const ScratchPermanent = mongoose.model('scratchPermenantSchema', scratchPermanentSchema);
 
 describe('Grouped Nested Resources (Counter)', function () {
   before(async () => {
@@ -105,5 +131,35 @@ describe('Grouped Nested Resources (Counter)', function () {
 
     doc.should.have.nested.property('children[1].title').and.equal('This is subtitle');
     doc.should.have.nested.property('children[1].slug').and.equal('this-is-subtitle-0001');
+  });
+
+  it('Update existing resource with additional child and check children are unique locally', async () => {
+    const doc = await ScratchPermanent.create({
+      title: 'This is a long title',
+      children: [
+        {
+          title: 'This is nested subtitle',
+        },
+        {
+          title: 'This is nested subtitle',
+        },
+      ],
+    });
+
+    doc.children.push(
+    {
+      title: 'This is nested subtitle',
+    });
+    await doc.save();
+
+
+    doc.should.have.nested.property('children[0].title').and.equal('This is nested subtitle');
+    doc.should.have.nested.property('children[0].slug').and.equal('this-is-nested-subtitle');
+
+    doc.should.have.nested.property('children[1].title').and.equal('This is nested subtitle');
+    doc.should.have.nested.property('children[1].slug').and.equal('this-is-nested-subtitle-0001');
+
+    doc.should.have.nested.property('children[2].title').and.equal('This is nested subtitle');
+    doc.should.have.nested.property('children[2].slug').and.equal('this-is-nested-subtitle-0002');
   });
 });
